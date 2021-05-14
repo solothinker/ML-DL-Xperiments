019e958a780c7d737958537cb6cefb27516cefef
Ashutosh Bhargava <ashutumho@gmail.com>
2021-05-15 00:51:52 +0530

    Editing the old script

--- Iris-Xperiments/KMeans_iris.py
+++ Iris-Xperiments/KMeans_iris.py
@@ -1,24 +1,33 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+
 from getIris import loadIris
 
+np.random.seed(404)
+
 df = loadIris()
-x,y = df[df.columns[:-1]],df[df.columns[-1]]
-xTrain,xTest,yTrain,yTest = train_test_split(x,y,test_size=0.1,shuffle=True)
 
-print(xTrain.shape,xTest.shape,yTrain.shape,yTest.shape)
+x, y = df[df.columns[:-1]], df[df.columns[-1]]
+xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.1, shuffle=True)
+print(xTrain.shape, xTest.shape, yTrain.shape, yTest.shape)
 
-kmeans = KMeans(n_clusters=3,random_state=0)
-kmeans.fit(xTrain,yTrain)
+kmeans = KMeans(n_clusters=3, init="k-means++", max_iter=1000, random_state=0)
+kmeans.fit(xTrain, yTrain)
 predict = kmeans.predict(xTest)
 
-print("Mean Square Error:      {:.5f}".format(mean_squared_error(yTest,predict)))
-print("Root Mean Square Error: {:.5f}".format(np.sqrt(mean_squared_error(yTest,predict))))
-print("Mean Absolute Error:    {:.5f}".format(mean_absolute_error(yTest,predict)))
+print("Mean Square Error:      {:.5f}".format(mean_squared_error(yTest, predict)))
+print("Root Mean Square Error: {:.5f}".format(np.sqrt(mean_squared_error(yTest, predict))))
+print("Mean Absolute Error:    {:.5f}".format(mean_absolute_error(yTest, predict)))
 
 print("Test Value:    {}".format(yTest.values))
 print("Predict Value: {}".format(predict))
+
+plt.step(np.arange(len(yTest.values)),yTest.values,'o', label="test")
+plt.step(np.arange(len(yTest.values)),predict,'x', label="predict")
+plt.legend()
+plt.show()
--- Iris-Xperiments/simpleReg_iris.py
+++ Iris-Xperiments/simpleReg_iris.py
@@ -1,22 +1,32 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 from sklearn import datasets
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error,mean_squared_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error,r2_score
 
 data = datasets.load_iris()
-xTrain,xTest,yTrain,yTest = train_test_split(data.data,data.target,
-                                             test_size=0.1,shuffle=True)
-print(xTrain.shape,xTest.shape,yTrain.shape,yTest.shape)
+xTrain, xTest, yTrain, yTest = train_test_split(
+    data.data, data.target, test_size=0.1, shuffle=True
+)
+print(xTrain.shape, xTest.shape, yTrain.shape, yTest.shape)
 
-lr = LinearRegression()
-lr.fit(xTrain,yTrain)
-predict = lr.predict(xTest)
+def linearRegression(Positive=False):
+    lr = LinearRegression(positive=Positive)
+    lr.fit(xTrain, yTrain)
+    predict = lr.predict(xTest)
 
-print("Mean Square Error:      {:.2f}".format(mean_squared_error(yTest,predict)))
-print("Root Mean Square Error: {:.2f}".format(np.sqrt(mean_squared_error(yTest,predict))))
-print("Mean Absolute Error:    {:.2f}".format(mean_absolute_error(yTest,predict)))
+    print("Mean Square Error:{:.2f}".format(mean_squared_error(yTest, predict)))
+    print("r2 Score:         {:.2f}".format(r2_score(yTest, predict)))
+    plt.figure()
+    plt.step(np.arange(len(yTest)),yTest,'o', label="test")
+    plt.step(np.arange(len(yTest)),predict,'x', label="predict")
+    plt.legend()
+    plt.show()
 
-
-
-
+print("Ordinary Least Square")
+linearRegression()
+print("---------------------")
+print("Non-Negative linear least square")
+linearRegression(Positive=True)
