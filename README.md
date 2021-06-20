2e9444339107ffaacfac22e19c57d7c3e8b70eb8
Ashutosh Bhargava <ashutumho@gmail.com>
2021-06-20 20:44:42 +0530

    Corrections in LR calculation
    LR - Linear Regression

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 ML & DL are the tools for the revolution in AI. Its requirements are to understand sophisticated mathematics and programming. Pre-build libraries give us the best result but to understand the functionality of these libraries, one has to jump in math and coding. 
 
 In this project, I am trying to implement the basic level of ML like  _y=mx+c_ and develop my understanding of how the basics work.   
-![simpleGif](/img/jtheta.PNG)
+![simpleGif](/img/mygif.gif)
 
 As well as playing the inbuilt data provided by different Python libraries. I think this will help to understand the core functionality of AI. Later, I am planning to play with the real data.
--- linearReg.py
+++ linearReg.py
@@ -1,25 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 plt.rcParams["figure.figsize"] = (20,10)
 np.random.seed(404)
 
 class LinearReg:
 
-    def __init__(self,alpha=0.0001):
+    def __init__(self,alpha=0.005):
         self.alpha=alpha
         
 
     def dataGen(self,theta=np.random.uniform(0,1,2),samples=500,plot=False):
         temp = dict()
         span=0.5
-        x=np.linspace(0,10,samples).reshape(samples,1)
         
+        x=np.linspace(0,10,samples).reshape(samples,1)
         y=theta[0] + theta[1]*x + np.random.uniform(-span,span,[samples,1])
+        
         temp['theta']=theta
         temp['x']=x
         temp['y']=y
         self.Data = temp
+        
         if plot:
             plt.scatter(x,y)
             plt.grid()
@@ -46,24 +49,9 @@ class LinearReg:
         for ii in x:
             temp = []
             for jj in y:
-                temp.append(self.JCal(jj,ii))
+                temp.append(self.JCal(ii,jj))
             z.append(temp)
         zz=np.matrix(z)
-##        if plot:
-##            fig = plt.figure()
-##            ax = plt.axes(projection='3d')
-##            ii,jj=self.Data['theta'][0],self.Data['theta'][1]
-##
-##            ax.scatter(ii,jj,self.JCal(xData,yData,jj,ii), c = 'r', marker='o',s=20,linewidth=2,label='Optimal Point')
-##            ax.scatter(xx, yy, zz,cmap='viridis', edgecolor='none')
-##
-##            ax.set_title('Surface plot')
-##            ax.set_xlabel('xx')
-##            ax.set_ylabel('yy')
-##            ax.set_zlabel('J theta')
-##
-##            plt.legend()
-##            plt.show()
             
         JSurf['xx']=xx
         JSurf['yy']=yy
@@ -73,7 +61,7 @@ class LinearReg:
     
     def Parameters(self):
         
-        tHat = np.random.uniform(0,1,2)
+        tHat = np.random.uniform(0,0.1,2)
 
         xData = self.Data['x']
         yData = self.Data['y']
@@ -81,34 +69,45 @@ class LinearReg:
         xx = self.JSurf['xx']
         yy = self.JSurf['yy']
         zz = self.JSurf['ZZ']
-        
+
         ii,jj=self.Data['theta'][0],self.Data['theta'][1]
         fig = plt.figure()
-        ax = plt.axes(projection='3d')
-        ax.scatter(jj,ii,self.JCal(ii,jj), c = 'r', marker='o',s=20,linewidth=2,label='Optimal Point')
-        for kk in range(1000):
-            #,label='Tuning Point')
-            tHat[0] -= self.alpha * np.sum(yData - (tHat[0]+tHat[1]*xData))/len(xData)
-            tHat[1] -= self.alpha * np.sum( (yData - (tHat[0]+tHat[1]*xData)) * xData )/len(xData)
-##            print(self.JCal(tHat[1],tHat[0]))
-##            if not kk%10:
-##                ax.scatter(tHat[0],tHat[1],self.JCal(tHat[1],tHat[0]), c = 'm', marker='o',s=20,linewidth=2)
-        
-        ax.scatter(xx, yy, zz,cmap='viridis', edgecolor='none')
-        ax.set_title('Surface plot')
-        ax.set_xlabel('xx')
-        ax.set_ylabel('yy')
-        ax.set_zlabel('J theta')
 
-        plt.legend()
-        plt.show()
+        ax = fig.add_subplot(121,projection='3d')
+        ax.scatter(jj,ii,self.JCal(ii,jj), c = 'r', marker='o',s=20,linewidth=2,label='Optimal Point')
+        ax.scatter(xx, yy, zz,cmap='viridis', edgecolor='none')
+
+        ax.set_title('Surface plot')
+        ax.set_xlabel('theta_1')
+        ax.set_ylabel('theta_0')
+        ax.set_zlabel('J theta')
+        ax.legend()
         
+        ax2 = fig.add_subplot(122)
+        ax2.scatter(xData,ii+jj*xData,label='actual plot')
+        ax2.legend()
+        ax2.grid()
+        
+
+        for kk in range(2500):
+
+            tHat[0] -= self.alpha * np.sum((tHat[0]+tHat[1]*xData)-yData)/len(xData)
+            tHat[1] -= self.alpha * np.sum( ((tHat[0]+tHat[1]*xData)-yData) * xData )/len(xData)
+
+            if not kk%100:
+                
+                ax.scatter(tHat[1],tHat[0],self.JCal(tHat[0],tHat[1]), c = 'm', marker='o',s=20,linewidth=2)
+                ax2.plot(xData,tHat[0]+tHat[1]*xData)
+                
+                plt.draw()
+                plt.pause(0.1)
+
         
         
 lr = LinearReg()
 data= lr.dataGen(plot=False)
 aa = lr.Jfun()
 lr.Parameters()
-##print(data)
+
         
         
